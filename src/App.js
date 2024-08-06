661ed1f10bc65d59e3e70b4f02dfc10afadd34be
Matin Amiri <matinpc.a@gmail.com>
2024-08-06 10:00:23 +0330

    upload to git hub

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ErrorInput from "./Error";
 
 function App() {
+  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordilInput] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  function clickHandler(e) {
+    e.preventDefault();
+    setClicked(true);
+    if (emailInput.length > 6 && passwordInput.length > 8) {
+      const newUser = {
+        email: emailInput,
+        password: passwordInput,
+      };
+      console.log(JSON.stringify(newUser));
+      setPasswordilInput("");
+      setEmailInput("");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>ورود به سایت</h2>
+      <div className="form">
+        <div className="Box">
+          <input
+            className="input"
+            type="email"
+            placeholder="ایمیل"
+            value={emailInput}
+            onChange={(e) => setEmailInput(e.target.value)}
+          />
+
+          {emailInput?.length > 6 && clicked && <ErrorInput text="ایمیل " />}
+        </div>
+        <div className="Box">
+          <input
+            className="input"
+            type={showPassword ? "text" : "password"}
+            placeholder="رمز عبور"
+            value={passwordInput}
+            onChange={(e) => setPasswordilInput(e.target.value)}
+          />
+          <span
+            className="showPassword"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {!showPassword ? "👁️" : "🫣"}
+          </span>
+
+          {passwordInput?.length > 8 && clicked && (
+            <ErrorInput text="رمز عبور " />
+          )}
+        </div>
+        <div className="button-box">
+          <button
+            type="submit"
+            className="suubmit-click"
+            onClick={clickHandler}
+          >
+            ورود
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Error.jsx
@@ -0,0 +1,5 @@
+function ErrorInput({ text }) {
+  return <p className="Error-input">لطفا {text}خود را به درستی وارد کنید </p>;
+}
+
+export default ErrorInput;
